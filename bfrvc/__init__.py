d5e695ceda0b048e7ee41ee7c8244813f3544b12
BFNF <festyus09@gmail.com>
2025-06-07 16:58:42 +0700

    Update __init__.py

--- bfrvc/__init__.py
+++ bfrvc/__init__.py
@@ -1,1 +1,1 @@
-from .core import run_infer_script, run_tts_script, prerequisites, run_batch_infer_script
+from .core import run_infer_script, run_tts_script, run_prerequisites_script, run_batch_infer_script
