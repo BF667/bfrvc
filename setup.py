d3a0cbffa6bc76a203579f930400e9cfdb59bcab
BF' sky Roger <festyus09@gmail.com>
2025-06-08 16:30:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bfrvc",
-    version="0.1.2",
+    version="0.2.2",
     description="A Retrieval-based Voice Conversion (RVC) fork",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
